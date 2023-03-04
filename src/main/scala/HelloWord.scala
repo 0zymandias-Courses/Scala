736ad90cfd2f06b18e390c1bc824726e635ccee0
Ivan Warlock <Ivan.Warlock@ibm.com>
2023-03-03 18:37:18 -0600

    HelloWorld & Mutability

---
+++ src/main/scala/HelloWord.scala
@@ -0,0 +1,7 @@
+
+
+object helloWorld {
+  def main(args: Array[String]) : Unit = {
+    println("Hello World !!!")
+  }// def main closure
+}// object main closure
---
+++ src/main/scala/variables.scala
@@ -0,0 +1,10 @@
+object variables {
+  def main(args: Array[String]) : Unit = {
+    println("Variables mutability in Scala !!!")
+    val constIntVal : Int  = 2147483647;
+    //shouldn't work, val variables are inmutable
+    //constIntVal = 2147483646;
+    var constIntVar : Int  = 2147483647;
+    constIntVar = 2147483647;
+  }// def main closure
+}// object closure
